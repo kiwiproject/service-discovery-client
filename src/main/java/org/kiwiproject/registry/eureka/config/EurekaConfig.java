032c29f0f9f9a2148dd78316f6543e3f9e53e7fd
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2020-10-02 13:52:40 -0400

    Add Eureka configs (#12)
    * EurekaConfig contains the base config for connecting to Eureka servers
    * EurekaRegistrationConfig is used for registering a service

---
+++ src/main/java/org/kiwiproject/registry/eureka/config/EurekaConfig.java
@@ -0,0 +1,42 @@
+package org.kiwiproject.registry.eureka.config;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.kiwiproject.net.KiwiUrls.replaceDomainsIn;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@Slf4j
+public class EurekaConfig {
+
+    /**
+     * A comma separated list of urls pointing to Eureka servers
+     */
+    protected String registryUrls;
+
+    /**
+     * Allows for adjusting {@code registryUrls} domain at runtime. This is useful if the urls are constant across services
+     * but a single service needs to access the server on a different domain due to networking restrictions
+     */
+    private String domainOverride;
+
+    @NotBlank
+    public String getRegistryUrls() {
+        var adjustedUrls = isBlank(domainOverride) ? registryUrls : replaceDomainsIn(registryUrls, domainOverride);
+        logWarningIfDomainOverrideIsSet();
+        return adjustedUrls;
+    }
+
+    private void logWarningIfDomainOverrideIsSet() {
+        if (isNotBlank(domainOverride)) {
+            LOG.warn("The 'domainOverride' parameter supersedes 'registryUrls'. Both are currently set in YAML config");
+        }
+    }
+
+}
---
+++ src/test/java/org/kiwiproject/registry/eureka/config/EurekaRegistrationConfigTest.java
@@ -0,0 +1,18 @@
+package org.kiwiproject.registry.eureka.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("EurekaRegistrationConfig")
+class EurekaRegistrationConfigTest {
+
+    @Test
+    void shouldDefaultProperties() {
+        var config = new EurekaRegistrationConfig();
+
+        assertThat(config.getExpirationIntervalInSeconds()).isEqualTo(EurekaRegistrationConfig.DEFAULT_LEASE_EXPIRATION_DURATION_SECONDS);
+        assertThat(config.getHeartbeatInvervalInSeconds()).isEqualTo(EurekaRegistrationConfig.DEFAULT_LEASE_RENEWAL_INTERVAL_SECONDS);
+    }
+}

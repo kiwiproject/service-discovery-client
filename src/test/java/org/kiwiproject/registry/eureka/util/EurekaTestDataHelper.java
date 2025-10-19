2383040313ea48076c121f7b5683d2a59ebbdc3e
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-10-18 22:30:48 -0400

    Revert "Fix missing comma"
    This reverts commit 3de906af4c263b2d279e4fc21b39cfe0949463cb.

--- src/test/java/org/kiwiproject/registry/eureka/util/EurekaTestDataHelper.java
+++ src/test/java/org/kiwiproject/registry/eureka/util/EurekaTestDataHelper.java
@@ -63,8 +63,11 @@ public class EurekaTestDataHelper {
                           "eureka": {
                             "client": {
                               "registerWithEureka": false,
-                              "fetchRegistry": false
-                            },
+                              "fetchRegistry": false,
+                              "serviceUrl": {
+                                "defaultZone": "http://127.0.0.1:8761/eureka/"
+                              }
+                            }
                             "instance": {
                               "hostname": "127.0.0.1",
                               "nonSecurePort": 8761
